3f985fb30854e2e1042dd1b17ab704d2a3f784e1
nitishjha18 <nitishjha1100@gmail.com>
2025-03-30 23:47:42 +0530

    Operatotors in Java Script

---
+++ first.js
@@ -0,0 +1,73 @@
+// Arithmatic Operator
+let a = 5;
+let b = 2;
+
+// ***********************Arithmetic Operator***********************
+// console.log("a+b =", a+b);
+// console.log("a-b =", a-b);
+// console.log("a=", a, "& b=", b);
+// console.log("a*b =", a*b);
+// console.log("a/b =", a/b);
+// console.log("a%b =", a%b);
+// console.log("a**b =", a**b); //  5^2
+
+
+// ***********************Unary Operaor*****************************
+// a++;      // a = a-1;
+// b--;      // b = b-1;
+// console.log("a=", a, "& b=", b);
+// Post ++
+// console.log("a++ =", a++); // first 5 is used
+// console.log("a =", a);     // now in this step new updated value is used
+// // Post --
+// console.log("a-- =", a--); // first 5 is used
+// console.log("a =", a);     // now in this step new updated value is used
+// // Pre ++
+// console.log("++a =", ++a); // first 5 is used
+// console.log("a =", a);     // now in this step new updated value is used
+// // Pre --
+// console.log("--a =", --a); // first 5 is used
+// console.log("a =", a);     // now in this step new updated value is used
+
+
+// ***********************Assignment Operators**********************
+// a+=4;  // a = a+4
+// console.log("+a=",a)
+// a-=4;  // a = 9
+// console.log("-a=",a)
+// a*=4;  // a = 5
+// console.log("*a=",a)
+// a/=4;  // a = 20
+// console.log("/a=",a)
+// a%=4;  // a = 4
+// console.log("%a=",a)
+// b**=4;  
+// console.log("**b=",b)
+
+
+// ***********************Comarision Operaters**********************
+// let c = "Bottle";
+// console.log("5 == 2   is ", a==b);    // returns true or false
+// console.log("a === c  is ", a===c);  // Strict checks data type also
+// console.log("5 != 2   is ", a!=b);
+// console.log("a !== c  is ", a!==c); 
+// >   Greter than
+// >=  Greter than equals to
+// <   Less than 
+// <=  Less than equals to
+
+// ***********************Lofgical Operater**********************
+let p = 6;
+let q = 5
+let cond1 = p > q;  // true
+let cond2 = p === 0;  // false
+// console.log("cond1 && cond2 =", cond1 && cond2);
+ console.log("cond1 || cond2 =", cond1 || cond2);
+//  a && b - a*b 
+//  a || b - a+b
+
+
+
+
+
+
