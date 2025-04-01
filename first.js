49202a37fc54288f4208eb279fcdd74aba3322c4
nitishjha18 <nitishjha1100@gmail.com>
2025-04-01 09:52:54 +0530

    Conditional Statements

--- first.js
+++ first.js
@@ -2,7 +2,7 @@
 let a = 5;
 let b = 2;
 
-// ***********************Arithmetic Operator***********************
+// ***************************************Arithmetic Operator***************************************
 // console.log("a+b =", a+b);
 // console.log("a-b =", a-b);
 // console.log("a=", a, "& b=", b);
@@ -12,7 +12,7 @@ let b = 2;
 // console.log("a**b =", a**b); //  5^2
 
 
-// ***********************Unary Operaor*****************************
+// ***************************************Unary Operaor*********************************************
 // a++;      // a = a-1;
 // b--;      // b = b-1;
 // console.log("a=", a, "& b=", b);
@@ -30,7 +30,7 @@ let b = 2;
 // console.log("a =", a);     // now in this step new updated value is used
 
 
-// ***********************Assignment Operators**********************
+// ***************************************Assignment Operators**************************************
 // a+=4;  // a = a+4
 // console.log("+a=",a)
 // a-=4;  // a = 9
@@ -45,7 +45,7 @@ let b = 2;
 // console.log("**b=",b)
 
 
-// ***********************Comarision Operaters**********************
+// **********************************************Comarision Operaters*******************************
 // let c = "Bottle";
 // console.log("5 == 2   is ", a==b);    // returns true or false
 // console.log("a === c  is ", a===c);  // Strict checks data type also
@@ -56,18 +56,76 @@ let b = 2;
 // <   Less than 
 // <=  Less than equals to
 
-// ***********************Lofgical Operater**********************
-let p = 6;
-let q = 5
-let cond1 = p > q;  // true
-let cond2 = p === 0;  // false
+// **********************************************Lofgical Operater**********************************
+// let p = 6;
+// let q = 5
+// let cond1 = p > q;  // true
+// let cond2 = p === 0;  // false
 // console.log("cond1 && cond2 =", cond1 && cond2);
- console.log("cond1 || cond2 =", cond1 || cond2);
+//  console.log("cond1 || cond2 =", cond1 || cond2);
 //  a && b - a*b 
 //  a || b - a+b
 
 
 
+// **********************************************Chapter-3 --> CONDITIONAL STATEMENT******************************
+ 
+// let age =26;
+// if(age >= 18) {
+// console.log("You can vote");     
+// }
+// if(age < 18){
+//     console.log("You can't vote");  
+// }
+
+// let mode = "pink";
+// let color;
+//  if(mode == "dark"){
+//     color = "black";
+//  }else{
+//     color = "white";
+//  }
+//   console.log(color);
+
+//  if(mode == "light"){
+//     color = "white";
+//  }
+//  console.log(color);
+// let num = 15;
+// if(num%2 == 0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+
+// }
+// let age = 2;
+// age >= 18 ? console.log("adult") : console.log("not adult");
+// // console.log(answer);
 
 
+// let num = prompt("Enter a number");
+// if(num % 5 == 0){
+//     console.log(num,"is a multiple of 5");
+// }else{
+//     console.log(num,"is not a multiple of 5");
+// }
 
+let score = prompt("Enter your score");
+let grade;
+if(score >= 90 && score <= 100){
+    grade="A";
+}else if(score >= 70 && score <= 89){
+    grade="B";
+}
+else if(score >= 60 && score <= 69){
+    grade="C";
+}
+else if(score >= 50 && score <= 59){
+    grade="A";
+}
+else if (score >= 0 && score <= 49){
+    grade="F"
+}
+console.log("Based on your score your grade is :", grade);
+
+ 
